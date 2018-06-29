c64e397b5620a85bf3df5c3534cb76a5fc2193ab
brianwilsonneudesic <brian.wilson@neudesic.com>
2018-06-29 14:49:41 -0500

    Initial Add

---
+++ Sample AI/Startup.cs
@@ -0,0 +1,184 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+
+namespace Sample_AI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // TODO: Change this to your image's path on your site. 
+            string imagePath = @"vision_images/monitor_small.jpg";
+
+            // Enable static files such as image files. 
+            app.UseStaticFiles();
+
+            string visionApiKey = Configuration["ComputerVisionAPI:ServiceKey"];
+            string visionApiEndPoint = Configuration["ComputerVisionAPI:ServiceEndPoint"];
+
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", visionApiKey);
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "visualFeatures=Categories,Description,Color&language=en";
+
+            // Assemble the URI for the REST API Call.
+            string uri = visionApiEndPoint + "/analyze" + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts an image you've added to your site's images folder. 
+            var fileInfo = env.WebRootFileProvider.GetFileInfo(imagePath);
+            byte[] byteData = GetImageAsByteArray(fileInfo.PhysicalPath);
+
+            string contentString = string.Empty;
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = client.PostAsync(uri, content).Result;
+
+                // Get the JSON response.
+                contentString = response.Content.ReadAsStringAsync().Result;
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("<h1>Cognitive Services Demo</h1>");
+                await context.Response.WriteAsync($"<p><b>Test Image:</b></p>");
+                await context.Response.WriteAsync($"<div><img style='max-width:100%' src=\"" + imagePath + "\" /></div>");
+                await context.Response.WriteAsync($"<p><b>Computer Vision API results:</b></p>");
+                await context.Response.WriteAsync("<p>");
+                await context.Response.WriteAsync(AnalyzeForHuman(contentString));
+                await context.Response.WriteAsync("<p>");
+            });
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        static string AnalyzeForHuman(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            if (json.IndexOf("human") > 0 || json.IndexOf("man") > 0 || json.IndexOf("woman") > 0) {
+                return "Found a person";
+            }
+            else
+            {
+                return JsonPrettyPrint(json);
+            }
+        }
+
+        /// <summary>
+        /// Formats the given JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string to format.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            string INDENT_STRING = "    ";
+            var indent = 0;
+            var quoted = false;
+            var sb = new StringBuilder();
+            for (var i = 0; i < json.Length; i++)
+            {
+                var ch = json[i];
+                switch (ch)
+                {
+                    case '{':
+                    case '[':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                        }
+                        sb.Append(ch);
+                        break;
+                    case '"':
+                        sb.Append(ch);
+                        bool escaped = false;
+                        var index = i;
+                        while (index > 0 && json[--index] == '\\')
+                            escaped = !escaped;
+                        if (!escaped)
+                            quoted = !quoted;
+                        break;
+                    case ',':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                        }
+                        break;
+                    case ':':
+                        sb.Append(ch);
+                        if (!quoted)
+                            sb.Append(" ");
+                        break;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
